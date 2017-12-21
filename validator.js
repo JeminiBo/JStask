79f270e5fee5caa918d842d8754cce67f95dd97e
JeminiBo <Joikervik@gmail.com>
2017-12-21 15:20:24 +0300

    Right Validator ver.6

--- validator.js
+++ validator.js
@@ -1,8 +1,7 @@
 
 function Validator(value) {
 
-    this.value = value;
-    this.check = true;
+    this.value = value || "";
     this.errorMessage = [];
     this.i = 0;
 }
@@ -11,107 +10,90 @@ function Validator(value) {
 
 Validator.prototype.isRequired = function () {
 
+    if (this.value.length == null) {
+        this.errorMessage[this.i] = "Значение отсутствует";
+        this.i++;
+    }
     return this;
 }
 Validator.prototype.maxLength = function (maxValue) {
 
-    if (this.value.length <= maxValue) {
-        this.check = true;
-    }
-    else {
-        this.check = false;
+    if (this.value.length > maxValue) {
         this.errorMessage[this.i] = "Максимальное количество символов " + maxValue;
         this.i++;
     }
-
-    console.log(this.check);
 
     return this;
 }
 Validator.prototype.minLength = function (minValue) {
 
-    if (this.value.length >= minValue) {
-        this.check = true;
-    }
-    else {
-        this.check = false;
+    if (this.value.length < minValue) {
         this.errorMessage[this.i] = "Минимальное количество сиволов " + minValue;
         this.i++;
     }
 
-    console.log(this.check);
+    return this;
+}
+Validator.prototype.max = function (max) {
+
+    if (this.value >= 110) {
+        this.errorMessage[this.i] = "Максимально возможное значение " + max;
+        this.i++;
+    }
 
     return this;
 }
-Validator.prototype.max = function () {
+Validator.prototype.min = function (min) {
 
-    if (this.value <= 110) {
-        this.check = true;
+    if (this.value <= 16) {
+        this.errorMessage[this.i] = "Минимально возможное значение " + min;
+        this.i++;
     }
-    else {
-        this.check = false;
-    }
+   
 
-    console.log(this.check);
-
-    return this;
-}
-Validator.prototype.min = function () {
-
-    if (this.value >= 16) {
-        this.check = true;
-    }
-    else {
-        this.check = false;
-    }
-
-    console.log(this.check);
     return this;
 }
 Validator.prototype.isEmail = function () {
 
     var emailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-    if (emailFormat.test(this.value)) {
-
-        this.check = true;
+    if (emailFormat.test(this.value) == false) {
+        this.errorMessage[this.i] = "Неверный формат Email ";
+        this.i++;
     }
-    else {
-        this.check = false;
-    }
-
-
-    console.log(this.check);
-
+    
     return this;
 }
 Validator.prototype.isInt = function () {
     
     if (isNaN(this.value) == true) {
-        this.check = false;
         this.errorMessage[this.i] = "Значение должно быть целым";
         this.i++;
     }
-
-    else {
-        this.check = true;
-       
-    }
-
-    console.log(this.check);
 
     return this;
 }
 
 Validator.prototype.isValid = function () {
 
-    for(var i = 0 ; i < this.errorMessage.length; i++)
+    
+    if (this.errorMessage.length == 0)
     {
-        console.log(this.errorMessage[i]);
+        return(true);
+    }
+    else 
+    {
+       
+
+        for(var i = 0 ; i < this.errorMessage.length; i++)
+        {
+            console.log(this.errorMessage[i]);
+        }
+        return(false);
     }
 
 }
 
-valid = new Validator("pas").minLength(5).maxLength(10).isInt();
+valid = new Validator("sds").minLength(5).maxLength(10).isInt();
 valid.isValid();
 
    
