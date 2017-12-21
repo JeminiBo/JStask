bd215374077f9c2a76b7e9b80758c30bd99a6797
JeminiBo <Joikervik@gmail.com>
2017-12-21 14:45:20 +0300

    Right Validator ver.4

--- validator.js
+++ validator.js
@@ -1,88 +1,103 @@
 
 function Validator(value) {
-       
+
     this.value = value;
     this.check = true;
+}
+
+
+
+Validator.prototype.isRequired = function () {
+
+    return this;
+}
+Validator.prototype.maxLength = function (maxValue) {
+
+    if (this.value.length <= maxValue) {
+        this.check = true;
+    }
+    else {
+        this.check = false;
+    }
+
+    console.log(this.check);
+
+    return this;
+}
+Validator.prototype.minLength = function (minValue) {
+
+    if (this.value.length >= minValue) {
+        this.check = true;
+    }
+    else {
+        this.check = false;
+    }
+
+    console.log(this.check);
+
+    return this;
+}
+Validator.prototype.max = function () {
+
+    if (this.value <= 110) {
+        this.check = true;
+    }
+    else {
+        this.check = false;
+    }
+
+    console.log(this.check);
+
+    return this;
+}
+Validator.prototype.min = function () {
+
+    if (this.value >= 16) {
+        this.check = true;
+    }
+    else {
+        this.check = false;
+    }
+
+    console.log(this.check);
+    return this;
+}
+Validator.prototype.isEmail = function () {
+
+    var emailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    if (emailFormat.test(this.value)) {
+
+        this.check = true;
+    }
+    else {
+        this.check = false;
     }
 
 
-   
-   Validator.prototype.isRequired = function () {
-    
-        return this;
-    }
-    Validator.prototype.maxLength = function (maxValue) {
-    
-        if (this.value <= maxValue )
-            this.check = true;
-        else
-            this.check = false;
-    
-        console.log(this.check);
-    
-        return this;
-    }
-    Validator.prototype.minLength = function (minValue) {
-        
-        if (this.value >= minValue)
-            this.check = true;
-        else
-            this.check = false;
-    
-        console.log(this.check);
-    
-        return this;
-    }
-    Validator.prototype.max = function () {
-    
-        if (this.value <= 110)
-            this.check = true;
-        else
-            this.check = false;
-    
-        console.log(this.check);
-    
-        return this;
-    }
-    Validator.prototype.min = function () {
-    
-        if (this.value >= 16)
-            this.check = true;
-        else
-            this.check = false;
-    
-        console.log(this.check);
-        return this;
-    }
-    Validator.prototype.isEmail = function () {
-    
-        var emailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-        if (emailFormat.test(this.value))
-            this.check = true;
-        else
-            this.check = false;
-    
-    
-        console.log(this.check);
-    
-        return this;
-    }
-    Validator.prototype.isInt = function () {
-        if (isNaN(this.value) == true)
-            this.check = false;
-    
-        else
-            this.check = true;
-    
-    
-        console.log(this.check);
-    
-        return this;
+    console.log(this.check);
+
+    return this;
+}
+Validator.prototype.isInt = function () {
+    if (isNaN(this.value) == true) {
+        this.check = false;
     }
 
- 
+    else {
+        this.check = true;
+    }
 
-    valid = new Validator(3).minLength(5).maxLength(10).isInt()
+    console.log(this.check);
+
+    return this;
+}
+
+Validator.prototype.isValid = function () {
+
+
+}
+
+valid = new Validator("pasha").minLength(5).maxLength(10).isInt();
 
    
 
