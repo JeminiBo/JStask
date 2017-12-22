af051ecb30740404a387d14cafc2bc276398bf6b
JeminiBo <Joikervik@gmail.com>
2017-12-22 12:14:25 +0300

    Add good test describe

--- tests.js
+++ tests.js
@@ -1,75 +1,109 @@
-describe("Check function minLength", function () {
-    it("It shoud return true, if test passed", function () {
+describe("Check Validator :", function(){
+describe("Check function minLength :", function () {
+    it("It shoud return true, if validation completed", function () {
         var valid = new Validator("sfsss").minLength(5);
         expect(valid.isValid()).toBe(true);
     });
+    it("It shoud return false, if validation failed", function () {
+        var valid = new Validator("sfss").minLength(5);
+        expect(valid.isValid()).toBe(false);
+    });
 });
 
 
-describe("Check function maxLength", function () {
-    it("It shoud return true, if test passed", function () {
+describe("Check function maxLength :", function () {
+    it("It shoud return true, if validation completed", function () {
         var valid = new Validator("sfsss").maxLength(10);
         expect(valid.isValid()).toBe(true);
     });
+    it("It shoud return false, if validation failed", function () {
+        var valid = new Validator("sfsssedsfsaf").maxLength(10);
+        expect(valid.isValid()).toBe(false);
+    });
 });
 
-describe("Check function isInt", function () {
-    it("It shoud return true, if test passed", function () {
+
+
+describe("Check function isInt :", function () {
+    it("It shoud return true, if validation completed", function () {
         var valid = new Validator("123").isInt();
         expect(valid.isValid()).toBe(true);
     });
+    it("It shoud return false, if validation failed", function () {
+        var valid = new Validator("1.5").isInt();
+        expect(valid.isValid()).toBe(false);
+    });
 });
-describe("Check function max", function () {
-    it("It shoud return true, if test passed", function () {
+
+describe("Check function max :", function () {
+    it("It shoud return true, if validation completed", function () {
         var valid = new Validator("123").max(150);
         expect(valid.isValid()).toBe(true);
     });
-});
-describe("Check function min", function () {
-    it("It shoud return true, if test passed", function () {
-        var valid = new Validator("dsa").min(5);
-        expect(valid.isValid()).toBe(true);
+    it("It shoud return false, if validation failed", function () {
+        var valid = new Validator("170").max(150);
+        expect(valid.isValid()).toBe(false);
     });
 });
 
-describe("Check function isEmail", function () {
-    it("It shoud return true, if test passed", function () {
+describe("Check function min :", function () {
+    it("It shoud return true, if validation completed", function () {
+        var valid = new Validator("10").min(5);
+        expect(valid.isValid()).toBe(true);
+    });
+    it("It shoud return false, if validation failed", function () {
+        var valid = new Validator("1").min(5);
+        expect(valid.isValid()).toBe(false);
+    });
+});
+
+
+describe("Check function isEmail :", function () {
+    it("It shoud return true, if validation completed", function () {
         var valid = new Validator("Joiekr@tut.by").isEmail();
         expect(valid.isValid()).toBe(true);
     });
+    it("It shoud return false, if validation failed", function () {
+        var valid = new Validator("Joiekrtut.by").isEmail();
+        expect(valid.isValid()).toBe(false);
+    });
 });
-describe("Check function isRequired", function () {
-    it("It shoud return true, if test passed", function () {
+
+describe("Check function isRequired :", function () {
+    it("It shoud return true, if validation completed", function () {
         var valid = new Validator("123").isRequired();
         expect(valid.isValid()).toBe(true);
     });
-});
-
-describe("Check function maxLength and minLength together", function () {
-    it("It shoud return true, if test passed", function () {
-        var valid = new Validator("sdsws").minLength(5).maxLength(10);
-        expect(valid.isValid()).toBe(true);
+    it("It shoud return false, if validation failed", function () {
+        var valid = new Validator("").isRequired();
+        expect(valid.isValid()).toBe(false);
     });
 });
 
-describe("Check function maxLength and minLength together", function () {
-    it("It shoud return false, if test passed", function () {
+
+describe("Check function maxLength and minLength together :", function () {
+    it("It shoud return true, if validation completed", function () {
+        var valid = new Validator("sdsws").minLength(5).maxLength(10);
+        expect(valid.isValid()).toBe(true);
+    });
+    it("It shoud return false, if validation failed", function () {
         var valid = new Validator("sds").minLength(5).maxLength(10);
         expect(valid.isValid()).toBe(false);
     });
 });
-describe("Check function min and isRequired together", function () {
-    it("It shoud return true, if test passed", function () {
+
+
+describe("Check function min and isRequired together :", function () {
+    it("It shoud return true, if validation completed", function () {
         var valid = new Validator("12").min(5).isRequired();
         expect(valid.isValid()).toBe(true);
     });
-});
-
-describe("Check function min and isRequired together", function () {
-    it("It shoud return false, if test passed", function () {
+    it("It shoud return false, if validation failed", function () {
         var valid = new Validator("1").min(5).isRequired();
         expect(valid.isValid()).toBe(false);
     });
 });
+});
+
 
 
--- validator.js
+++ validator.js
@@ -37,7 +37,7 @@ Validator.prototype.minLength = function (minValue) {
 }
 Validator.prototype.max = function (max) {
     if (isNaN(Number(this.value)) == true) {
-        this.errorMessage.push("Значение должно быть числом " );
+        this.errorMessage.push("Значение должно быть числом");
     }
     if (this.value >= max) {
         this.errorMessage.push("Максимально возможное значение " + max);
@@ -48,7 +48,7 @@ Validator.prototype.max = function (max) {
 }
 Validator.prototype.min = function (min) {
     if (isNaN(Number(this.value)) == true) {
-        this.errorMessage.push("Значение должно быть числом " );
+        this.errorMessage.push("Значение должно быть числом");
     }
     if (this.value <= min) {
         this.errorMessage.push("Минимально возможное значение " + min);
@@ -70,7 +70,11 @@ Validator.prototype.isEmail = function () {
 }
 Validator.prototype.isInt = function () {
 
-    if (isNaN(this.value) == true) {
+    if (isNaN(Number(this.value)) == true) {
+        this.errorMessage.push("Значение должно быть числом");
+    }
+
+    if (parseInt(this.value) != this.value) {
         this.errorMessage.push("Значение должно быть целым");
 
     }
